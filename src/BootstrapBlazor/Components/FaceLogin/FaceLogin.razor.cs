997b135bc15806b812c96d479d21587d626b5aa1
Argo-PD <argo@163.com>
2020-09-29 12:17:58 +0800

    feat: 增加 Baidu 人脸 AI 组件

---
+++ src/BootstrapBlazor/Components/FaceLogin/FaceLogin.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootstrapBlazor.Components
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed partial class FaceLogin
+    {
+        private ElementReference FaceElement { get; set; }
+
+        /// <summary>
+        /// OnAfterRenderAsync 方法
+        /// </summary>
+        /// <param name="firstRender"></param>
+        /// <returns></returns>
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            if (firstRender) await JSRuntime.InvokeVoidAsync(FaceElement, "bb_face");
+        }
+
+        private async Task OnRegister()
+        {
+            await BaiduFaceHttpClient.Register();
+        }
+    }
+}
