f0fe5b15fa848effe83088048abc362308581ac0
Argo-PD <argo@163.com>
2020-09-29 12:18:36 +0800

    doc: 增加 Baidu 人脸 AI 文档

---
+++ src/BootstrapBlazor.Shared/Pages/FaceLogins.razor.cs
@@ -0,0 +1,32 @@
+﻿using BootstrapBlazor.Shared.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BootstrapBlazor.Shared.Pages
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public sealed partial class FaceLogins
+    {
+        private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
+        {
+            // TODO: 移动到数据库中
+            new AttributeItem() {
+                Name = "Text",
+                Description = "页脚组件显示的文字",
+                Type = "string",
+                ValueList = " — ",
+                DefaultValue = " — "
+            },
+            new AttributeItem() {
+                Name = "Target",
+                Description = "页脚组件控制的滚动条组件 ID",
+                Type = "string",
+                ValueList = " — ",
+                DefaultValue = " — "
+            }
+        };
+    }
+}
