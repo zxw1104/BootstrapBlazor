1e71885ed702462f266300b44f3ab4d5549504a5
Argo-Lenovo <argo@163.com>
2022-03-06 15:14:30 +0800

    test: 增加 Drag 单元测试

---
+++ test/UnitTest/Components/DragDropTest.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+namespace UnitTest.Components;
+
+public class DragDropTest : BootstrapBlazorTestBase
+{
+    [Fact]
+    public void Drag_Ok()
+    {
+        var cut = Context.RenderComponent<Dropzone<string>>(pb =>
+        {
+            pb.Add(a => a.Items, new List<string> { "1", "2" });
+        });
+    }
+}
