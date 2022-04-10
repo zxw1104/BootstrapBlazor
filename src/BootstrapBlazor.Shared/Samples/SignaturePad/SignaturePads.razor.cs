79c87eaf3b1585fb356bb796249ec84bdc0017af
Argo-Lenovo <argo@163.com>
2022-04-10 18:22:57 +0800

    doc: 格式化文档

--- src/BootstrapBlazor.Shared/Samples/SignaturePad/SignaturePads.razor.cs
+++ src/BootstrapBlazor.Shared/Samples/SignaturePad/SignaturePads.razor.cs
@@ -50,38 +50,141 @@ public sealed partial class SignaturePads
         return Task.CompletedTask;
     } 
 
+    ///// <summary>
+    ///// 获得属性方法
+    ///// </summary>
+    ///// <returns></returns>
+    //private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
+    //{
+    //    new AttributeItem("EnableSaveBase64Btn","启用保存为base64按钮",  "true","bool") ,
+    //    new AttributeItem("EnableSavePNGBtn","启用保存为PNG按钮文本",  "false","bool") ,
+    //    new AttributeItem("EnableSaveJPGBtn","启用保存为JPG按钮文本",  "false","bool") ,
+    //    new AttributeItem("EnableSaveSVGBtn","启用保存为SVG按钮文本",  "false","bool") ,
+
+    //    new AttributeItem("CssClass","组件CSS式样",  "signature-pad-body") ,
+    //    new AttributeItem("BtnCssClass","按钮CSS式样",  "btn btn-light") ,
+    //    new AttributeItem("Responsive","响应式css界面,为所有用户设计最佳体验",  "false","bool") ,
+    //    new AttributeItem("BackgroundColor","组件背景",  "rgb(255, 255, 255),设置 rgba(0,0,0,0)为透明") ,
+    //};
+
     /// <summary>
     /// 获得属性方法
     /// </summary>
     /// <returns></returns>
-    private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
+    private static IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
     {
-        new AttributeItem("OnResult","签名结果回调方法",  "","Func<string, Task>?") ,
-        new AttributeItem("OnAlert","手写签名警告信息回调",  "","Func<string, Task>?") ,
-        new AttributeItem("OnError","错误回调方法",  "","Func<string, Task>?") ,
-        new AttributeItem("OnClose","手写签名关闭信息回调",  "","Func<Task>?") ,
-
-        new AttributeItem("SignAboveLabel","在框内签名标签文本",  "在框内签名") ,
-        new AttributeItem("ClearBtnTitle","清除按钮文本",  "清除") ,
-        new AttributeItem("SignatureAlertText","请先签名提示文本",  "请先签名") ,
-        new AttributeItem("ChangeColorBtnTitle","换颜色按钮文本",  "换颜色") ,
-        new AttributeItem("UndoBtnTitle","撤消按钮文本",  "撤消") ,
-        new AttributeItem("SaveBase64BtnTitle","保存为base64按钮文本",  "确定") ,
-        new AttributeItem("SavePNGBtnTitle","保存为PNG按钮文本",  "PNG") ,
-        new AttributeItem("SaveJPGBtnTitle","保存为JPG按钮文本",  "JPG") ,
-        new AttributeItem("SaveSVGBtnTitle","保存为SVG按钮文本",  "SVG") ,
-
-        new AttributeItem("EnableChangeColorBtn","启用换颜色按钮",  "true","bool") ,
-        new AttributeItem("EnableAlertJS","启用JS错误弹窗",  "true","bool") ,
-        new AttributeItem("EnableSaveBase64Btn","启用保存为base64按钮",  "true","bool") ,
-        new AttributeItem("EnableSavePNGBtn","启用保存为PNG按钮文本",  "false","bool") ,
-        new AttributeItem("EnableSaveJPGBtn","启用保存为JPG按钮文本",  "false","bool") ,
-        new AttributeItem("EnableSaveSVGBtn","启用保存为SVG按钮文本",  "false","bool") ,
-
-        new AttributeItem("CssClass","组件CSS式样",  "signature-pad-body") ,
-        new AttributeItem("BtnCssClass","按钮CSS式样",  "btn btn-light") ,
-        new AttributeItem("Responsive","响应式css界面,为所有用户设计最佳体验",  "false","bool") ,
-        new AttributeItem("BackgroundColor","组件背景",  "rgb(255, 255, 255),设置 rgba(0,0,0,0)为透明") ,
+        // TODO: 移动到数据库中
+        new AttributeItem() {
+            Name = "OnResult",
+            Description = "签名结果回调方法",
+            Type = "Func<string, Task>?",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "OnAlert",
+            Description = "手写签名警告信息回调",
+            Type = "Func<string, Task>?",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "OnError",
+            Description = "错误回调方法",
+            Type = "Func<string, Task>?",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "OnClose",
+            Description = "手写签名关闭信息回调",
+            Type = "Func<string, Task>?",
+            ValueList = " — ",
+            DefaultValue = " — "
+        },
+        new AttributeItem() {
+            Name = "SignAboveLabel",
+            Description = "在框内签名标签文本",
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "在框内签名"
+        },
+        new AttributeItem() {
+            Name = "ClearBtnTitle",
+            Description = "清除按钮文本",
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "清除"
+        },
+        new AttributeItem() {
+            Name = "SignatureAlertText",
+            Description = "请先签名提示文本",
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "请先签名"
+        },
+        new AttributeItem() {
+            Name = "ChangeColorBtnTitle",
+            Description = "换颜色按钮文本",
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "换颜色"
+        },
+        new AttributeItem() {
+            Name = "ChangeColorBtnTitle",
+            Description = "换颜色按钮文本",
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "换颜色"
+        },
+        new AttributeItem() {
+            Name = "UndoBtnTitle",
+            Description = "撤消按钮文本",
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "撤消"
+        },
+        new AttributeItem() {
+            Name = "SaveBase64BtnTitle",
+            Description = "保存为 base64 按钮文本",
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "确定"
+        },
+        new AttributeItem() {
+            Name = "SavePNGBtnTitle",
+            Description = "保存为 PNG 按钮文本",
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "PNG"
+        },
+        new AttributeItem() {
+            Name = "SaveJPGBtnTitle",
+            Description = "保存为 JPG 按钮文本",
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "JPG"
+        },
+        new AttributeItem() {
+            Name = "SaveSVGBtnTitle",
+            Description = "保存为 SVG 按钮文本",
+            Type = "string",
+            ValueList = " — ",
+            DefaultValue = "SVG"
+        },
+        new AttributeItem() {
+            Name = "EnableChangeColorBtn",
+            Description = "启用换颜色按钮",
+            Type = "bool",
+            ValueList = " — ",
+            DefaultValue = "true"
+        },
+        new AttributeItem() {
+            Name = "EnableAlertJS",
+            Description = "启用 JS 错误弹窗",
+            Type = "bool",
+            ValueList = " — ",
+            DefaultValue = "true"
+        }
     };
-
 }
